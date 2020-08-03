eda1f967a916dd3f2badffb1a47628dba6379eaa
asier cacheiro <asier-26@hotmail.com>
2020-08-03 21:25:16 +0200

    Agregar archivos de proyecto.

---
+++ apiweb/Data/apiwebContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using apiweb;
+
+namespace apiweb.Data
+{
+    public class apiwebContext : DbContext
+    {
+        public apiwebContext (DbContextOptions<apiwebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<apiweb.Customer> Customer { get; set; }
+        public DbSet<apiweb.Customer> GetCustomerData { get; }
+        public DbSet<apiweb.Customer> GetAllCustomers { get; }
+    }
+}
---
+++ apiweb/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace apiweb
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public int PostalCode { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public DateTime Date { get; set; }
+        public Boolean Eliminado { get; set; }
+    }
+}
